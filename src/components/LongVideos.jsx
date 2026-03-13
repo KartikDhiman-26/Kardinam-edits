5a38c91ea70568f2f88a90c4eb022aef1971a014
KartikDhiman-26 <kartikdhiman80@gmail.com>
2026-03-13 16:53:19 +0530

    Long videos patch

--- src/components/LongVideos.jsx
+++ src/components/LongVideos.jsx
@@ -3,21 +3,19 @@ import { motion } from "framer-motion"
 import VideoCard from "./VideoCard"
 import VideoModal from "./VideoModal"
 
-export default function ReelsSection() {
+export default function LongVideos() {
 
   const [activeVideo, setActiveVideo] = useState(null)
 
-  const reels = [
-    "1173248849",
-    "1173249028",
-    "1173249437",
-    "1173249320",
-    "1173249554",
-    "1173249636",
+  const videos = [
+    "uo-p_cnnt3w",
+    "o5ZoPPHgW3U",
+    "8AIKWYoAQLA",
+    "D0cpcEu3juE",
   ]
 
   return (
-    <section id="works" className="py-40 max-w-7xl mx-auto px-8">
+    <section className="py-40 max-w-7xl mx-auto px-8">
 
       <motion.h2
         initial={{ opacity: 0, y: 40 }}
@@ -26,25 +24,25 @@ export default function ReelsSection() {
         viewport={{ once: true }}
         className="text-5xl font-bold text-center mb-20"
       >
-        Short Form Edits
+        Long Form Projects
       </motion.h2>
 
-      <div className="grid grid-cols-2 md:grid-cols-3 gap-10">
-        {reels.map((video, i) => (
+      <div className="grid md:grid-cols-2 gap-14">
+        {videos.map((video, i) => (
           <VideoCard
             key={i}
             src={video}
             setActiveVideo={setActiveVideo}
-            ratio="9:16"
-            platform="vimeo"
+            ratio="16:9"
+            platform="youtube"
           />
         ))}
       </div>
 
       <VideoModal
         video={activeVideo}
-        ratio="9:16"
-        platform="vimeo"
+        ratio="16:9"
+        platform="youtube"
         close={() => setActiveVideo(null)}
       />
 
